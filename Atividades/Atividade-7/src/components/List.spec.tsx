0c1ba361a4a20a01e084785f92c8b99c74dced82
Gabriel da Silva <72719459+gbrl-exe@users.noreply.github.com>
2022-11-07 19:33:20 -0300

    Correção dos erros dos testes 2 e 3

--- Atividades/Atividade-7/src/components/List.spec.tsx
+++ Atividades/Atividade-7/src/components/List.spec.tsx
@@ -1,44 +1,44 @@
-import {render, waitFor, screen } from '@testing-library/react'
+import { render, waitFor, screen } from '@testing-library/react'
 import userEvent from '@testing-library/user-event'
-import List from './List'
+import List from './List' 
 
 describe('List Component', () => {
-    it('should render list items', async () => {
-        const {getByText, rerender} = render(<List initialItems={['Diego', 'Rodz', 'Mayk']}/>)
+  it('should render list items', () => {
+      const { getByText } = render(<List initialItems={['Diego', 'Rodz', 'Mayk']}/>)
 
-        expect(getByText('Diego')).toBeInTheDocument()
-        expect(getByText('Rodz')).toBeInTheDocument()
-        expect(getByText('Mayk')).toBeInTheDocument()
-        
-        rerender(<List initialItems={['Gabriel']}/>)
+      expect(getByText('Diego')).toBeTruthy()
+      expect(getByText('Rodz')).toBeTruthy()
+      expect(getByText('Mayk')).toBeTruthy()
+  })
 
-        expect(screen.getByText('Diego')).toBeInTheDocument 
-        expect(screen.getByText('Mayk')).toBeInTheDocument 
+
+  it('should be able to add new item to the list', async () => {
+    const { getByText, getByPlaceholderText, findByText } = render(<List initialItems={[]} />)
+
+    const inputElement = getByPlaceholderText('Novo item');
+    const addButton = getByText('Adicionar');
+
+    await userEvent.type(inputElement, 'Novo');
+    await userEvent.click(addButton);
+
+    await waitFor(() => {
+      expect(getByText('Novo')).toBeInTheDocument()
     })
+  });
 
-    it('should be able to add new item to the list', async () => {
-        const {getByText, getByPlaceholderText} = render(<List initialItems={[]}/>)
+  it('should be able to remove item to the list', async () => {
+    const { 
+        getByText, 
+        getAllByText, 
+        queryByText
+    } = render(<List initialItems={['Diego']}/>)
 
-        const inputElement = getByPlaceholderText('Novo item')
-        const addButton = getByText('Adicionar');
+    const removeButton = getAllByText('Remover')
+    
+    userEvent.click(removeButton[0])
 
-        userEvent.type(inputElement, 'Novo')
-        userEvent.click(addButton);
-        
-        await waitFor(() => {
-            expect(getByText('Novo')).toBeInTheDocument()       
-        })
+    await waitFor(() => {
+        expect(queryByText('Diego')).not.toBeTruthy()
     })
-
-    it('should be able to remove an item from the list', async () => {
-        const {getAllByAltText , queryByText} = render(<List initialItems={['Diego']}/>)
-
-        const removeButtons = getAllByAltText('Remover')
-
-        userEvent.click(removeButtons[0]);
-        
-        await waitFor(() => {
-            expect(queryByText('Diego')).not.toBeInTheDocument()       
-        })
-    })
-})
+  });
+});
