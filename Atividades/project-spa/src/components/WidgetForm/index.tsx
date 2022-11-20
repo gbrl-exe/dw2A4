be0e4bcebaaa94f0858d23d38da4169ecde93a88
Gabriel da Silva <72719459+gbrl-exe@users.noreply.github.com>
2022-11-20 17:19:08 -0300

    Inclusão do botão de avaliação

--- Atividades/project-spa/src/components/WidgetForm/index.tsx
+++ Atividades/project-spa/src/components/WidgetForm/index.tsx
@@ -2,6 +2,7 @@ import { useState } from "react"
 import bugImageUrl from '../../assets/bug.svg'
 import ideaImageUrl from '../../assets/idea.svg'
 import thoughtImageUrl from '../../assets/thought.svg'
+import starImageUrl from '../../assets/star.svg'
 import { FeedbackTypeStep } from "./Steps/FeedbackTypeStep"
 import { FeedbackContentStep } from "./Steps/FeedbackContentStep"
 import { FeedbackSuccessStep } from "./Steps/FeedbackSuccessStep"
@@ -18,6 +19,14 @@ export const feedbackTypes = {
         title:'Ideia',
         image: {
             source: ideaImageUrl,
+            alt: 'Imagem de uma lâmpada'
+        }
+        
+    },
+    AVALIATION: {
+        title:'Avaliation',
+        image: {
+            source: starImageUrl,
             alt: 'Imagem de uma lâmpada'
         }
         
@@ -43,7 +52,7 @@ export function WidgetForm() {
     }
 
     return (
-        <div className='bg-zinc-900 p-4 relative rounded-2xl mb-4 flex flex-col items-center shadow-lg w-[calc(100vw-2rem)] md:w-auto'>
+        <div className='bg-zinc-900 p-4 relative rounded-2xl mb-4 flex flex-col items-center shadow-lg w-[calc(100vw-2rem)] md:w-auto'>         
             
             {feedbackSent ? (
                 <FeedbackSuccessStep onFeedbackRestartRequested={handleRestartFeedback}/>
@@ -61,7 +70,7 @@ export function WidgetForm() {
                     )}
                 </>
             )}
-                        
+            
             <footer className="text-xs text-neutral-400 mb-1">
                 Feito por <a className="underline underline-offset-2" href="https://linkedin.com/in/gabriel-dba">Gabriel</a>
             </footer>
